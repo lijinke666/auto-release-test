97632918b8fd8824b4e23cfb1d702c4510af384e
Jinke Li <a1231236677287@163.com>
2021-10-16 15:39:32 +0000

    fix: xx

--- packages/module-a/index.js
+++ packages/module-a/index.js
@@ -3,6 +3,6 @@ module.exports = {
     console.log('module A 1111');
   },
   fix() {
-    console.log('module a bug fix, 并且修改了父级的一些文件');
+    console.log('module a bug fix, 并且修改了父级的一些文件111');
   },
 };
