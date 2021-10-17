5856f580da88036eb57b981738a8858edebb1268
Jinke Li <a1231236677287@163.com>
2021-10-17 09:55:58 +0000

    chore(release): publish

--- packages/module-a/index.js
+++ packages/module-a/index.js
@@ -3,6 +3,6 @@ module.exports = {
     console.log('module A 1111');
   },
   fix() {
-    console.log('module a bug fix, 并且修改了父级的一些文件111 1.2.4');
+    console.log('module a bug fix, 并且修改了父级的一些文件111 1.2.5');
   },
 };
